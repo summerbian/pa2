810e9d77d4ceb5ca958df129f58a43afa68ca2be
xiaoxia bian <bianxx_cn@hotmail.com>
2017-11-25 16:02:43 -0500

    utils tcp checksum

--- router/sr_utils.h
+++ router/sr_utils.h
@@ -29,6 +29,7 @@
 #define SR_UTILS_H
 
 uint16_t cksum(const void *_data, int len);
+uint32_t tcp_cksum(sr_ip_hdr_t *, sr_tcp_hdr_t *, int);
 
 uint16_t ethertype(uint8_t *buf);
 uint8_t ip_protocol(uint8_t *buf);
