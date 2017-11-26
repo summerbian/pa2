8a4a95999f2f3f1bc03783f2df163241bffbe71c
xiaoxia bian <bianxx_cn@hotmail.com>
2017-11-26 02:58:18 -0500

    add library

--- router/sr_nat.c
+++ router/sr_nat.c
@@ -1,12 +1,13 @@
 
 #include <signal.h>
 #include <assert.h>
-#include "sr_nat.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include "sr_nat.h"
+#include "sr_utils.h"
 
 int sr_nat_init(struct sr_nat *nat) { /* Initializes the nat */
 
